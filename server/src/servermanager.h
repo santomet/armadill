5406e6e72243043bab9aef867ab4ce6bf9424faf
TeslaN <teslan223@gmail.com>
2016-03-21 02:45:09 +0100

    trying the other way around this shit

--- server/src/servermanager.h
+++ server/src/servermanager.h
@@ -13,11 +13,24 @@ private:
 public:
     ServerManager(QString path) : clientDatabase(path) {}
 
+    /*!
+     * \brief newRegistration               Registers a new user
+     * \param nickName
+     * \param passwordHash
+     * \return                              True if successfully registered, false if something went wrong/user already exists
+     */
     bool newRegistration(QString nickName, QString passwordHash);
 
+    /*!
+     * \brief login                         Tries to authentify new user and set his state as logged in
+     * \param nickname
+     * \param password
+     * \param address                       IP/Domain of client
+     * \param port                          client's port
+     * \param cert                          base64-formatted certificate signing request
+     * \return                              true if passwords matches and everything goes fine, false if not
+     */
     bool login(QString nickname, QString password, QString address, int port, QString cert);
-
-
 
 	/*!
 	* \brief exportOnlineUsersJson        returns a JSON of online users
